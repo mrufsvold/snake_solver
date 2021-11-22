cd216a1b3e0dec957232d483f4ebaa3cbead88d8
Micah Rufsvold <mjrufsvold@mathematica-mpr.com>
2021-11-21 23:20:21 -0500

    Feat: add dispathed structs for game objs

--- src/structures.jl
+++ src/structures.jl
@@ -1,23 +1,34 @@
 using DataFrames: DataFrame, select
 using DataFramesMeta
 using DataFramesMeta: @subset!, @transform!
-import YAML
-include("utils.jl")
 
-config = YAML.load_file("config.yml")
-grid_size = get(config, "grid_size", 3)
+struct x_move
+    val::Int
+end
 
-rows = repeat_items(1:grid_size,grid_size)
-cols = repeat(1:grid_size,grid_size)
+struct y_move
+    val::Int
+end
 
-adjacency_df = DataFrame(N1_Row= repeat(rows,grid_size^2),
-                        N1_Col = repeat(cols, grid_size^2),
-                        N2_Row= repeat_items(rows,grid_size^2),
-                        N2_Col= repeat_items(cols,grid_size^2)
-                        )
+# Change Head in x or y direction
+function move_head(move::x_move)
+    (snake.head[1] + move, snake.head[2])
+end
 
-# Remove identity pairs from node comparisons
-@subset!(adjacency_df, .!((:N1_Row .== :N2_Row) .& (:N1_Col .== :N2_Col)))
+function move_head(move::y_move)
+    (snake.head[1], snake.head[2]+ move)
+end
+
+struct snake
+    head::AbstractVector{Int}
+    tail::AbstractVector{AbstractVector{Int}}
+end
+
+struct game
+    grid_size::Int
+    board::DataFrame
+    snake::snake
+end
 
 function calc_node_adjacncy(r1::AbstractArray,c1::AbstractArray,r2::AbstractArray,c2::AbstractArray)
     (abs.(r1 .- r2) .== 1) .& (abs.(c1 .- c2) .== 0) .| (abs.(r1 .- r2) .== 0) .& (abs.(c1 .- c2) .== 1)
