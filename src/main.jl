45630fd755592d579dc2473b7146eca17972e7fd
Micah Rufsvold <mjrufsvold@mathematica-mpr.com>
2021-11-22 22:38:54 -0500

    Feat: add init conditions to main script

--- src/main.jl
+++ src/main.jl
@@ -1,8 +1,19 @@
-using DataFrames: DataFrame
-using DataFramesMeta: @subset
-
-
+using Pkg
+Pkg.activate("../snake_solver")
+using Revise
 include("initial_setup.jl")
+
+snake_pos = [1,0,0,0]
+apple_pos = [0,1,0,0]
+initial_board[:, :Snake] = snake_pos
+initial_board[:, :Apple] = apple_pos
+
+initial_board
+
 include("structures.jl")
 
-    
+move = x_move(1)
+my_snake = snake([1,1])
+my_game = game(initial_board,my_snake)
+
+new_status = update_board(move, my_game)
