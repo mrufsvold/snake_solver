a7e56be502cdbf945093ecaeddf0554efe538e9c
Micah Rufsvold <mjrufsvold@mathematica-mpr.com>
2021-11-22 22:39:42 -0500

    Fix: Updates to make types match

--- src/structures.jl
+++ src/structures.jl
@@ -1,7 +1,11 @@
-using DataFrames: DataFrame, select
+using DataFrames
 using DataFramesMeta
-using DataFramesMeta: @subset!, @transform!
+using DataFramesMeta: @subset!, @transform!, @eachrow
+using Random
 
+
+
+###### Movement Options
 struct x_move
     val::Int
 end
@@ -10,64 +14,86 @@ struct y_move
     val::Int
 end
 
-# Change Head in x or y direction
-function move_head(move::x_move)
-    (snake.head[1] + move, snake.head[2])
+function move_head(move::x_move, head::AbstractVector{Int})
+    [head[1], head[2]+ move.val]    
 end
 
-function move_head(move::y_move)
-    (snake.head[1], snake.head[2]+ move)
+function move_head(move::y_move, head::AbstractVector{Int})
+    [head[1] + move.val, head[2]]
 end
+
+
+##### Game Objects ######
 
 struct snake
-    head::AbstractVector{Int}
-    tail::AbstractVector{AbstractVector{Int}}
+    body::AbstractVector{AbstractVector{Int}}
+    head::AbstractVector
+    head::NamedTuple
 end
+
+snake(start_location::AbstractVector{Int}) = snake([start_location], start_location)
+snake(body::AbstractVector{AbstractVector{Int}}) = snake(body, last(body))
 
 struct game
-    grid_size::Int
     board::DataFrame
     snake::snake    
-
-    # Update the board
-    function update_board(move)
-        new_head = move_head(move)
-
-        ### Check for fails ###
-        # Try to find new location on board, error if out of bounds
-        try 
-            new_node = @subset(board, (:Row .== new_head[1]) .& (:Col .== new_head[2]))
-        catch e
-            error("Out of Bounds!")
-        end
-
-        # create new tail for snake movement
-        new_tail = deepcopy(snake.tail)
-        new_board = 
-        # If snake body already at this node, error
-        if  new_node[1,:Snake] == 1 
-            error("The snake ate itself!")
-        # Check if snake got an apple
-        elseif new_node[1, :Apple] == 1
-            #Do snake gets longer, place new Apple
-            push!(new_tail, snake.head)
-            push!(new_tail, new_head)
-            @transform(board, :Snake = [[x.Row,x.Col] in new_tail for x in copy.(eachrow([:Row, :Col]))])
-            #=
-            turn row and col into a 2 elem vector 
-            check if vector is in tail=#
-        # Move the snake
-        else
-            #Do move snake
-            popfirst!(new_tail)
-            push!(new_tail, snake.head)
-        end
-    end
 end
-    
 
+#### Base Functions ####
 
+function update_board(move,game)
+    new_head = move_head(move, game.snake.head)
 
+    ### Check for fails ###
+    # Try to find new location on board, error if out of bounds
+    try 
+        new_node = @subset(game.board, (:Row .== new_head[1]) .& (:Col .== new_head[2]))
+    catch e
+        error("Out of Bounds!")
+    end
+    new_node = @subset(game.board, (:Row .== new_head[1]) .& (:Col .== new_head[2]))
+    println(new_node)
+    println(typeof(new_node))
+    println(new_node[1, :Apple])
+    # create new body and board to record snake movement
+    new_body::AbstractVector{AbstractVector{Int}} = deepcopy(game.snake.body)
+    new_board::DataFrame = deepcopy(game.board)
+
+    # If snake body already at this node, error
+    if  new_node[1,:Snake] == 1 
+        error("The snake ate itself!")
+
+    # Check if snake got an apple
+    elseif new_node[1, :Apple] == 1
+        push!(new_body, new_head)
+        @transform!(new_board, :Snake = [[x.Row,x.Col] in new_body for x in copy.(eachrow([:Row, :Col]))])
+        
+        # Construct a new apple column with the apple in a random empty node
+        empty_node_count = nrow(@subset(new_board, :Snake .== 0))
+        apple_placement = rand(collect(1:empty_node_count))
+        apple_col = []
+        n = 1
+        for is_snake in new_board[:,:Snake]
+            if is_snake == 0 && n == apple_placement
+                push!(apple_col, 1)
+                apple_col = [apple_col; zeros(Int, length(new_board)-length(apple_col))]
+                break
+            end
+            n += 1
+            push!(apple_col, 0)
+        end
+        new_board[:,:Apple] = apple_col
+
+    # Move the snake
+    else
+        #Do move snake
+        popfirst!(new_body)
+        push!(new_body, new_head)
+        @transform!(new_board, :Snake = [[x.Row,x.Col] in new_body for x in copy.(eachrow([:Row, :Col]))])
+    end
+    new_snake::snake = snake(new_body)
+    return game(new_board,new_snake)
+end
 
 
 
