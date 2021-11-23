7ed33b13181ac4e0bc86912d37361439579f2774
mrufsvold <86363075+mrufsvold@users.noreply.github.com>
2021-11-23 12:18:09 -0500

    Feat: complete snake and apple movement

--- src/structures.jl
+++ src/structures.jl
@@ -10,7 +10,7 @@ struct Node
     row::Int
     col::Int
 end
-
+    
 struct snake
     """Represent the snake's location"""
     body::AbstractVector{Node}
@@ -44,77 +44,66 @@ function move_head(move::y_move, head::Node)
     Node(head.row +1, head.col)
 end
 
-
-##### Game Objects ######
-
-struct snake
-    body::AbstractVector{AbstractVector{Int}}
-    head::AbstractVector
-    head::NamedTuple
-end
-
-snake(start_location::AbstractVector{Int}) = snake([start_location], start_location)
-snake(body::AbstractVector{AbstractVector{Int}}) = snake(body, last(body))
-
-struct game
-    board::DataFrame
-    snake::snake    
-end
-
 #### Base Functions ####
 
-function update_board(move,game)
-    new_head = move_head(move, game.snake.head)
+function move_snake(move, current_snake::snake, ate_apple::Bool)
+    new_body = [deepcopy(current_snake.body); deepcopy(current_snake.head)]
+    new_head = move_head(move, current_snake.head)
+    if ! ate_apple
+        popfirst!(new_body)
+    end
+    snake(new_body, new_head)
+end
 
+function move_apple(current_board::DataFrame)
+    empty_node_count = nrow(
+        current_board[(current_board[!,:Snake].== 0), :]
+    )
+    new_apple_placement = zeros(empty_node_count)
+    new_apple_placement[rand(collect(1:empty_node_count))] = 1
+    current_board[(current_board[!,:Snake].== 0), :Apple] = new_apple_placement
+    current_board
+end
+
+function update_board(move,current_game_state)
+    
+    new_snake = move_snake(move, current_game_state.snake, current_game_state.ate_apple)
+    new_board = deepcopy(current_game_state.board)
+    # Delete Snake tail on board
+    if ! current_game_state.ate_apple
+        snake_tail = current_game_state.snake.body[1]
+        new_board[((new_board[!,:Row] .== snake_tail.row) .&
+            (new_board[!,:Col] .== snake_tail.col)),:Snake] = [0] 
+    end
+        
     ### Check for fails ###
     # Try to find new location on board, error if out of bounds
-    try 
-        new_node = @subset(game.board, (:Row .== new_head[1]) .& (:Col .== new_head[2]))
-    catch e
+    new_node_row = new_board[
+        (new_board[!,:Row] .== new_snake.head.row) .& 
+        (new_board[!,:Col] .== new_snake.head.col),:]
+    if nrow(new_node_row) == 0
         error("Out of Bounds!")
-    end
-    new_node = @subset(game.board, (:Row .== new_head[1]) .& (:Col .== new_head[2]))
-    println(new_node)
-    println(typeof(new_node))
-    println(new_node[1, :Apple])
-    # create new body and board to record snake movement
-    new_body::AbstractVector{AbstractVector{Int}} = deepcopy(game.snake.body)
-    new_board::DataFrame = deepcopy(game.board)
-
     # If snake body already at this node, error
-    if  new_node[1,:Snake] == 1 
-        error("The snake ate itself!")
-
-    # Check if snake got an apple
-    elseif new_node[1, :Apple] == 1
-        push!(new_body, new_head)
-        @transform!(new_board, :Snake = [[x.Row,x.Col] in new_body for x in copy.(eachrow([:Row, :Col]))])
-        
-        # Construct a new apple column with the apple in a random empty node
-        empty_node_count = nrow(@subset(new_board, :Snake .== 0))
-        apple_placement = rand(collect(1:empty_node_count))
-        apple_col = []
-        n = 1
-        for is_snake in new_board[:,:Snake]
-            if is_snake == 0 && n == apple_placement
-                push!(apple_col, 1)
-                apple_col = [apple_col; zeros(Int, length(new_board)-length(apple_col))]
-                break
-            end
-            n += 1
-            push!(apple_col, 0)
+    elseif  new_node_row[1,:Snake] == 1
+        # If the board is filled with snake, you win!
+        if nrow(current_game_state.board) < length(new_snake.body)
+            error("You win!")
         end
-        new_board[:,:Apple] = apple_col
-
-    # Move the snake
-    else
-        #Do move snake
-        popfirst!(new_body)
-        push!(new_body, new_head)
-        @transform!(new_board, :Snake = [[x.Row,x.Col] in new_body for x in copy.(eachrow([:Row, :Col]))])
+        error("The snake ate itself!")
     end
-    new_snake::snake = snake(new_body)
-    return game(new_board,new_snake)
+    
+    # Check if snake got an apple
+    ate_apple = new_node_row[1, :Apple] == 1
+    
+    if ate_apple
+        new_board = move_apple(new_board)
+    end
+    
+    # Add new_head to board
+    new_board[
+        ((new_board[!,:Row] .== new_snake.head.row) .& 
+        (new_board[!,:Col] .== new_snake.head.col)),:Snake] = [1]
+    game_state(new_board, new_snake, ate_apple)
 end
 
 
