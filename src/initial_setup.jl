480a0f2e90e9338b12836aca1b7c2489644de6e3
Micah Rufsvold <mjrufsvold@mathematica-mpr.com>
2021-11-24 10:36:30 -0500

    Refactor: wrap adjanency df in function

--- src/initial_setup.jl
+++ src/initial_setup.jl
@@ -11,6 +11,7 @@ grid_size = get_grid_size()
 rows = repeat_items(1:grid_size,grid_size)
 cols = repeat(1:grid_size,grid_size)
 
+function create_adjacency_df()
 adjacency_df = DataFrame(N1_Row= repeat(rows,grid_size^2),
                         N1_Col = repeat(cols, grid_size^2),
                         N2_Row= repeat_items(rows,grid_size^2),
@@ -20,11 +21,12 @@ adjacency_df = DataFrame(N1_Row= repeat(rows,grid_size^2),
 # Remove identity pairs from node comparisons
 @subset!(adjacency_df, .!((:N1_Row .== :N2_Row) .& (:N1_Col .== :N2_Col)))
 
-function calc_node_adjacncy(r1::AbstractArray,c1::AbstractArray,r2::AbstractArray,c2::AbstractArray)
+    function calc_node_adjacency(r1::AbstractArray,c1::AbstractArray,r2::AbstractArray,c2::AbstractArray)
     (abs.(r1 .- r2) .== 1) .& (abs.(c1 .- c2) .== 0) .| (abs.(r1 .- r2) .== 0) .& (abs.(c1 .- c2) .== 1)
 end
 
-@transform!(adjacency_df, :adjacent = calc_node_adjacncy(:N1_Row, :N1_Col, :N2_Row, :N2_Col))
+    @transform!(adjacency_df, :adjacent = calc_node_adjacency(:N1_Row, :N1_Col, :N2_Row, :N2_Col))
+end
 
 function create_initial_state()
     initial_board = DataFrame(Row= rows, Col=cols, Snake = 0, Apple = 0)
