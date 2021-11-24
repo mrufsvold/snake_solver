96da5e28f8e1cf54cdc7c70baa84b91c589cc735
Micah Rufsvold <mjrufsvold@mathematica-mpr.com>
2021-11-23 22:35:09 -0500

    Feat: Add visualizer to main loop

--- src/main.jl
+++ src/main.jl
@@ -3,14 +3,20 @@ Pkg.activate(".")
 using Revise
 includet("initial_setup.jl")
 includet("structures.jl")
+includet("visualize.jl")
 
 my_game = create_initial_state()
 
+moves = (y_move(-1), y_move(1))
+
 game_record = []
-for _ in 1:4
-    move = x_move(1)
+for m in 1:6
+    sleep(1)
+    move = moves[m%2+1]
+    @show move
     global my_game
     my_game = update_board(move, my_game)
+    draw_window(my_game.snake.body,my_game.apple)
     push!(game_record, my_game)
 end
 
