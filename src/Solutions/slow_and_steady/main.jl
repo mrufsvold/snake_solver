b21c8ff601ddade62011dcc895e2db947f333ba2
Micah Rufsvold <mjrufsvold@mathematica-mpr.com>
2021-11-24 10:41:23 -0500

    Feat: Add logic to solution

--- src/Solutions/slow_and_steady/main.jl
+++ src/Solutions/slow_and_steady/main.jl
@@ -3,6 +3,24 @@ includet("../../structures.jl")
 
 function handler(current_game_state::game_state, response::Response)
 
+    move::Union{x_move,y_move} = x_move(0)
+    head = current_game_state.snake.head
+    if head.row == 1 && head.col ==1
+        move = y_move(-1)
+    elseif head.row == 2 && head.col % 2 == 0 && head.col != response.grid_size
+        move = x_move(1)
+    elseif head.row == 1
+        move = x_move(-1)
+    elseif head.col % 2 == 0
+        move = y_move(1)
+    elseif head.row == response.grid_size && head.col % 2 == 1
+        move = x_move(1)
+    elseif head.col % 2 == 1
+        move = y_move(-1)
+    else
+        error("You missed a case for $head")
+    end
 
+    response.move = move
     return response
 end
